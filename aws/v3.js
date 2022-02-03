51275c91765f5a4555376ec97b1aecd056403fc3
Nate W <18470121+papimuerte@users.noreply.github.com>
2022-02-03 10:47:19 -0700

    Update v3.js

--- aws/v3.js
+++ aws/v3.js
@@ -1,7 +1,7 @@
 const { S3 } = require('@aws-sdk/client-s3');
 
 const aws = new S3({
-  endpoint: sfo3.digitaloceanspaces.com,
+  endpoint: https://sfo3.digitaloceanspaces.com,
   region: us-east-1,
   accessKeyId: process.env.DO_SPACES_KEY,
   secretAccessKey: process.env.DO_SPACES_SECRET,
